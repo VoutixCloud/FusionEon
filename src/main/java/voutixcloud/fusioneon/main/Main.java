90e14598ce696631a15045ca1010aba6cf1f1c38
Fruxz <cedricspitzer@outlook.de>
2020-02-25 14:01:05 +0100

    Basic structure initialization

---
+++ src/main/java/voutixcloud/fusioneon/main/Main.java
@@ -0,0 +1,16 @@
+package voutixcloud.fusioneon.main;
+
+/**
+ * This class helps us, to manage the start of our application
+ *
+ * @author Fruxz
+ * @version 1.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: 25.02.2020 Application start
+        System.out.println("registered fusioneon start...");
+
+    }
+}
